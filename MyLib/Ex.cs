5094d6dc66610f08dc9f1ee64e38cf4f28080fe7
Artem Mikhailob <mobail_2003@bk.ru>
2022-07-06 16:57:34 +0300

    homework_8 ver.1

---
+++ MyLib/Ex.cs
@@ -0,0 +1,43 @@
+﻿namespace MyLib;
+using System;
+using static System.Console;
+public class Ex
+{
+    public static int [,] GetArray (int m, int n)
+    {
+        int [,] result=new int [m, n];
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                result[i,j]=new Random().Next(-100,101);
+            }
+        }
+    return result;
+    }
+
+    public static void PrintArray (int [,] result)
+    {
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Write($"{result[i, j]} \t ");
+            }
+            WriteLine();
+        }
+    }
+
+    public static void PrintArray (double [] result)
+    {
+        for (int i = 0; i < result.Length; i++)
+        {
+
+            Write($"{result[i]} \t ");
+            WriteLine();
+        }
+    }
+
+}
+
+
---
+++ task_54/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 54.** Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Исходный массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+*/
+
+using static System.Console;
+using MyLib;
+using System.Linq;
+
+Clear();
+WriteLine("Введите размер матрицы через пробел: ");
+string[] sizeS = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int m = int.Parse(sizeS[0]);
+int n = int.Parse(sizeS[1]);
+int[,] mainArray =Ex.GetArray(m, n);
+Ex.PrintArray(mainArray);
+ArrangeRow (mainArray);
+WriteLine("=========");
+Ex.PrintArray(mainArray);
+
+
+
+
+void ArrangeRow (int [,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        int [] arr = new int [array.GetLength(1)];
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            arr [j]=array[i,j];
+        }
+        arr=arr.OrderBy(x => x).ToArray;
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            array[i,j]=arr[j];
+        }
+    }
+}
